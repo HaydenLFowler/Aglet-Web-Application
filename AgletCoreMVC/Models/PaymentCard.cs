0ef893e53d654d3ebf31ebeab24b42561c20ed2c
Hayden Fowler <hayd_4@live.co.uk>
2019-11-21 11:42:23 +0000

    Add annotations

--- AgletCoreMVC/Models/PaymentCard.cs
+++ AgletCoreMVC/Models/PaymentCard.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 /// <summary>
 /// When placing an order the user must provide payment information,
 /// this can apply to any staff that decide to also be a customer as well.
@@ -10,10 +11,15 @@ namespace AgletCoreMVC.Models
 public class PaymentCard 
     {
 	public string PaymentCardID { get; set; }
+
+    [Required]
 	public int CardNumber { get; set;}
+
 	public int CustomerID;
 	public int SecurityCode {get; set;}
 	public string ExpiryDate {get; set;}
+
+        // Navigation
 
 	public virtual User UserID { get; set; }
 
