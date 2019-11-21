1691047fd64f3d9027d2ef0d4d90ba391563ed17
Conor bailey <cpbailey025@gmail.com>
2019-11-21 11:26:24 +0000

    Added namespace

--- AgletCoreMVC/Models/Lace.cs
+++ AgletCoreMVC/Models/Lace.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 /// <summary>
 /// Conor
 /// </summary>
-public class Lace {
-    public int LaceID{get; set;}
-	public string Colour { get; set; }
-    public string Print { get; set; }
-    public string Brand { get; set; }
-    public double Length { get; set; }
-    public string Image { get; set; }
-    public string Description { get; set; }
 
-    private ICollection<Item> Item { get; set; }
+namespace AgletCoreMVC.Models
+{
+    public class Lace
+    {
+        public int LaceID { get; set; }
+        public string Colour { get; set; }
+        public string Print { get; set; }
+        public string Brand { get; set; }
+        public double Length { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+        private ICollection<Item> Item { get; set; }
+    }
 }
