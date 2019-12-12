6a311adf90ff82ab6cefdb0a520cbfd02b665198
Hayden Fowler <8>
2019-12-12 11:33:22 +0000

    Initialiser added

---
+++ AgletCoreMVC/Data/DbInitiliser.cs
@@ -0,0 +1,44 @@
+﻿using AgletCoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgletCoreMVC.Data
+{
+    public static class DbInitialiser
+    {
+        public static void Initialise (ApplicationDbContext context)
+        {
+            //InitialiseAddresses(context);
+            //InitialiseUsers(context);
+        }
+
+        private static void InitialiseUsers(ApplicationDbContext context)
+        {
+        }
+
+        private static void InitialiseAddresses(ApplicationDbContext context)
+        {
+            if(context.Address.Any())
+            {
+                return;
+            }
+
+            var addresses = new Address[]
+            {
+                new Address
+                {
+                    StreetAddress = "High Street"
+                }
+            };
+
+            foreach (Address a in addresses)
+            {
+                context.Address.Add(a);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
