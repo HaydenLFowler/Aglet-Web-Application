96010391bd185ba6bc001d2a9daff9d07699eb12
Hayden Fowler <8>
2020-01-16 10:10:38 +0000

    Everything

--- AgletCoreMVC/Controllers/HomeController.cs
+++ AgletCoreMVC/Controllers/HomeController.cs
@@ -5,14 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AgletCoreMVC.Models;
+using AgletCoreMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AgletCoreMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Laces()
+        {
+            ViewData["Message"] = "Your Laces list page.";
+
+            return View(await _context.Lace.ToListAsync());
         }
 
         public IActionResult About()
